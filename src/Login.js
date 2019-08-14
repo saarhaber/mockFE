72a91b7f23dace53d874ab888c87e349c66267fe
mehrabha <mehrabhfz@gmail.com>
2019-08-14 14:09:34 -0400

    Designed login form

--- src/Login.js
+++ src/Login.js
@@ -1,29 +1,30 @@
 import React from "react";
 import {Form, Button} from 'react-bootstrap';
+import "./Login.css"
 
 class Login extends React.Component {
   render() {
     return (
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <div className="login-container">
+        <Form className="login-form">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control className="form-element" type="email" placeholder="username" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicChecbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control className="form-element" type="password" placeholder="password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicChecbox">
+            <Form.Check type="checkbox" label="Remember Me" />
+          </Form.Group>
+          <Button className="form-element" variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
