7dce7dfb1382350100f879d97c875b02a066a9ac
saarhaber <saarhaber@gmail.com>
2019-08-15 12:59:50 -0400

    user is added, mapes, and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import './App.css';
-import {Redirect} from 'react-router';
+// import {Redirect} from 'react-router';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <Redirect to="/signup"/>
-  );
+import Login from './components/Login';
+import Signup from './components/Signup';
+import User from './components/User';
+import SingleInterview from './components/SingleInterview';
+import dashboard from './components/dashboard';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+
+
+class App extends React.Component {
+
+render(){
+ return (
+    //UNCOMMENT THIS LATER
+    // <Redirect to="/signup"/>
+    
+console.log(this.props.users),
+<Router>
+      <div>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        {/* the singleInterview link is temporary so we can see how the components looks like */}
+        <Route path="/SingleInterview" component={SingleInterview} />
+        <Route path="/dashboard" component={dashboard} />
+        {this.props.users.map(user => {
+              return (
+                <Route exact path={"/user/" + user.id}
+                  render={() => {
+                    return (
+                      <User
+                        user_id={user.id}
+                        user_firstName={user.firstName}
+                        user_lastName={user.lastName}
+                        user_imageUrl={user.imageUrl}
+                        user_email={user.email}
+                        user_organization={user.organization}
+                        user_description={user.description}
+                        user_profession={user.profession}
+                        user_interviewAmount={user.interviewAmount}
+                        user_lastInterview={user.lastInterview}
+                        user_isInterviewer={user.isInterviewer}
+                        />
+                    )
+                  }}
+                />
+              )
+            })}
+      </div>
+    </Router> 
+    );
+}
+}
+const getStateToProps = (state) => {
+  return {
+    users: state.users
+  }
 }
 
-export default App;
+export default connect(getStateToProps)(App)
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,46 @@
 import {combineReducers} from 'redux';
 
+const users_ = [
+  {
+    id:1,
+    firstName: "Ajani",
+    lastName: "Stewart",
+    imageUrl: "https://media.licdn.com/dms/image/C4E03AQEJu9M_1KwiSQ/profile-displayphoto-shrink_800_800/0?e=1571270400&v=beta&t=K1hQTxvdOAuEk23OMrM232t-bx3l4sx12UxLazK_vUc",
+    email: "email@email.com",
+    organization: "Hunter College",
+    description: "Student Web Developer paitentily waiting for the heat death of the universe",
+    profession: "student",
+    interviewAmount: 3,
+    lastInterview: (new Date()).toISOString(),
+    isInterviewer: false
+  },
+  {    id:2,
+    firstName: "John",
+    lastName: "Cline",
+    imageUrl: 'http://3.bp.blogspot.com/-hrebbdHVnMs/T_xetr5DAHI/AAAAAAAAAzo/CODjoTjZh7Y/s1600/cat+10.jpg',
+    email: "john@cline.com",
+    organization: "Google",
+    description: "Big Brain",
+    profession: "web developer",
+    interviewAmount: 10,
+    lastInterview: (Date.now()/1000),
+    isInterviewer: true
+  },
+  {    id:3,
+    firstName: "Kim",
+    lastName: "Wong",
+    imageUrl: null,
+    email: "kim.wong@email.com",
+    organization: "Bloomberg",
+    description: "Bigger Brain",
+    profession: "software engineer",
+    interviewAmount: 8,
+    lastInterview: (Date.now()/1000),
+    isInterviewer: true
+  }]
+
 // reducer: list of users
-const userReducer = (users = [], action) => {
+const userReducer = (users = users_, action) => {
   switch(action.type) {
     case "ADD_USER":
       users.push(action.payload);
