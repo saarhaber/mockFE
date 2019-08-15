a5a218e95626623788cbcf479b6b12fbdb6df2aa
mehrabha <mehrabhfz@gmail.com>
2019-08-14 21:38:44 -0400

    Input error handeling

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,58 +6,114 @@ import './Login.css';
 import './Signup.css';
 
 class Signup extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      redirect: false
+      redirect: false,
+      errorCodes: [],
     }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
+  errors = [
+    "Unknown",
+    "Passwords do not match",
+    "Invalid Password",
+    "Account must have an email",
+    "First name cannot be empty",
+    "Last name cannot be empty",
+    "Select student or employee"
+  ];
+
   handleSubmit(e) {
     e.preventDefault();
     console.log("Submitted");
-    const login_success = (
-      e.target.username.value == this.state.username &&
-      e.target.password.value == this.state.password
-    );
 
-    if (login_success) {
-      this.setState({redirect: true});
-    } else {
-      this.setState({loginFailed: true});
+    let errorCodes = [];
+
+    let newUser = {
+      firstname: undefined,
+      lastname: undefined,
+      email: undefined,
+      password: undefined,
+      isEmployer: undefined
     }
-  }
-  
-  error = {
-    1: "Passwords do not match",
-    2: "Enter a valid email address",
-    3: "First name cannot be empty",
-    4: "Last name cannot be empty"
+
+    // Check inputs
+    if(e.target.firstname.value) {
+      newUser.firstname= e.target.firstname.value;
+    } else {
+      errorCodes.push(4);
+    }
+
+    if(e.target.lastname.value) {
+      newUser.lastname= e.target.lastname.value;
+    } else {
+      errorCodes.push(5);
+    }
+
+    if(e.target.email.value) {
+      newUser.email= e.target.email.value;
+    } else {
+      errorCodes.push(3);
+    }
+
+    // Check passwords
+    if(e.target.password.value) {
+      if (e.target.confirm.value != e.target.password.value) {
+        errorCodes.push(1);
+      } else {
+        newUser.password = e.target.password.value;
+      }
+    } else {
+      errorCodes.push(2);
+    }
+
+    if(e.target.formRadio.value) {
+      newUser.isEmployer= e.target.formRadio.value;
+    } else {
+      errorCodes.push(6);
+    }
+
+    // If a valid input is received, create user
+    if (errorCodes.length == 0) {
+      this.setState({redirect: true});
+
+    } else {
+      // Show 2 errors
+      if (errorCodes.length > 2) {
+        errorCodes = errorCodes.slice(0, 2);
+      }
+      this.setState({errorCodes: errorCodes})
+    }
   }
 
   render() {
+    console.log("Users: ", this.props.users);
+    console.log("User: ", this.props.user);
+
     if (this.state.redirect) {
       return(
-        <Redirect to="/dashboard"/>
+        <Redirect to="/login"/>
       );
     }
+
     return (
       <div className="Login">
         <Card className="login-card">
           <Card.Header>Create an Account</Card.Header>
           <Card.Body>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Row>
                 <Form.Group as={Col}>
-                  <Form.Control type="name" name="firstname" placeholder="First name" />
+                  <Form.Control type="name" name="firstname" placeholder="First Name" />
                 </Form.Group>
                 <Form.Group as={Col}>
-                  <Form.Control type="name" name="lastname" placeholder="Last name" />
+                  <Form.Control type="name" name="lastname" placeholder="Last Name" />
                 </Form.Group>
               </Form.Row>
               <Form.Group controlId="formBasicEmail">
-                <Form.Control name="email" placeholder="Email" />
+                <Form.Control name="email" type="email" placeholder="Email" />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone.
                 </Form.Text>
@@ -67,25 +123,32 @@ class Signup extends React.Component {
                   <Form.Control className="form-element" name="password" type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group as={Col} controlId="formBasicPassword">
-                  <Form.Control className="form-element" name="confirm" type="password" placeholder="Confirm password" />
+                  <Form.Control className="form-element" name="confirm" type="password" placeholder="Confirm Password" />
                 </Form.Group>
               </Form.Row>
               <div className="radio-btns">
                 <Form.Check
                     className="text-muted"
+                    value={1}
                     type="radio"
                     label="Employer"
-                    name="formHorizontalRadios"
+                    name="formRadio"
                     id="formHorizontalRadios1"
                 />
                 <Form.Check
                     className="text-muted"
+                    value={0}
                     type="radio"
                     label="Student"
-                    name="formHorizontalRadios"
+                    name="formRadio"
                     id="formHorizontalRadios2"
                 />
               </div>
+              {this.state.errorCodes.map(errorCode => (
+                <Alert variant={"warning"}>
+                  {this.errors[errorCode]}
+                </Alert>
+              ))}
               <Button variant="primary" type="submit">
                 Submit
               </Button>
@@ -103,4 +166,4 @@ const getStateToProps = (state) => {
     user: state.user
   }
 }
-export default connect()(Signup);
+export default connect(getStateToProps)(Signup);
