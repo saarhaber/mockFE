ac5715955440ad96ea84d379d9e620771f469da8
mehrabha <mehrabhfz@gmail.com>
2019-08-15 16:06:02 -0400

    connected dashboard to redux store

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Dashboard.css';
 import SingleInterview from './SingleInterview';
+import {connect} from 'react-redux';
+import { statement } from '@babel/template';
+
+const TAG = "DASHBOARD_JS";
 
 class Dashboard extends React.Component {
   render () {
+    console.log(TAG, "All interviews", this.props.interviews);
+    console.log(TAG, "Selected interview", this.props.interview);
     return (
       <div>
         <div className="heading">
@@ -23,4 +29,11 @@ class Dashboard extends React.Component {
   }
 }
 
-export default Dashboard;
+const getStateToProps = (state) => {
+  return {
+    interviews: state.interviews,
+    interview: state.interview
+  }
+}
+
+export default connect(getStateToProps, {})(Dashboard);
