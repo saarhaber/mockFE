4c3ea3f1d7be79210a674843c7a2b98f04fadcdb
mehrabha <mehrabhfz@gmail.com>
2019-08-19 14:23:42 -0400

    editform functions

--- src/components/EditAccount.js
+++ src/components/EditAccount.js
@@ -15,10 +15,31 @@ class EditAccount extends React.Component {
     super(props);
     this.state = {
       redirect: false,
-      errorCodes: [],
+      profileErrors: [],
+      contactErrors: [],
+      passwordErrors: [],
+      id: 1,
+      firstName: "Ajani",
+      lastName: "Stewart",
+      imageUrl: "http://i.imgur.com/AItCxSs.jpg",
+      email: "ajani@gmail.com",
+      organization: "Hunter College",
+      description: "Student Web Developer paitentily waiting for the heat death of the universe",
+      profession: "student",
+      interviewAmount: 3,
+      lastInterview: "2019-08-19T15:46:24.399Z",
+      isInterviewer: false
     }
-    //this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  states = [
+    "AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC",  
+    "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA",  
+    "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",  
+    "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC",  
+    "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"
+  ];
 
   errors = [
     "Unknown",
@@ -29,6 +50,76 @@ class EditAccount extends React.Component {
     "Last name cannot be empty",
     "Select student or employee"
   ];
+
+  profileSubmit(e) {
+    e.preventDefault();
+
+    let errorCodes = [];
+    // Check inputs
+    if(e.target.firstName.value) {
+    } else {
+      errorCodes.push(4);
+    }
+
+    if(this.state.lastName) {
+    } else {
+      errorCodes.push(5);
+    }
+
+    if (errorCodes.length == 0) {
+    } else {
+      // Show 2 errors
+      if (errorCodes.length > 2) {
+        errorCodes = errorCodes.slice(0, 2);
+      }
+      this.setState({profileErrors: errorCodes})
+    }
+  }
+  
+  contactSubmit(e) {
+    e.preventDefault();
+
+    // Check inputs
+    let errorCodes= [];
+    if(e.target.email.value) {
+    } else {
+      errorCodes.push(3);
+    }
+
+    if (errorCodes.length == 0) {
+    } else {
+      // Show 2 errors
+      if (errorCodes.length > 2) {
+        errorCodes = errorCodes.slice(0, 2);
+      }
+      this.setState({contactErrors: errorCodes})
+    }  
+  }
+
+  passwordSubmit(e) {
+    e.preventDefault();
+
+    // Check inputs
+    let errorCodes= [];
+
+    if(e.target.password.value == e.target.confirm.value) {
+    } else {
+      errorCodes.push(1);
+    }
+
+    if (errorCodes.length == 0) {
+    } else {
+      // Show 2 errors
+      if (errorCodes.length > 2) {
+        errorCodes = errorCodes.slice(0, 2);
+      }
+      this.setState({passwordErrors: errorCodes})
+    }  
+  }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
   render() {
     if (this.state.redirect) {
@@ -44,57 +135,61 @@ class EditAccount extends React.Component {
           <Card.Body>
             <Tabs defaultActiveKey="profile">
               <Tab eventKey="profile" title="Profile" className="profileTab">
-                <Form>
+                <Form onSubmit={this.profileSubmit.bind(this)}>
                   <Form.Row>
                     <Form.Group as={Col}>
                       <Form.Label>First Name</Form.Label>
-                      <Form.Control type="name" />
+                      <Form.Control name="firstName" type="name" value={this.state.firstName} onChange={this.handleChange}/>
                     </Form.Group>
                     <Form.Group as={Col}>
                       <Form.Label>Last Name</Form.Label>
-                      <Form.Control type="name" />
+                      <Form.Control name="lastName" type="name" value={this.state.lastName} onChange={this.handleChange}/>
                     </Form.Group>
                   </Form.Row>
 
                   <Form.Row>
                     <Form.Group as={Col} controlId="formGridCity">
                       <Form.Label>City</Form.Label>
-                      <Form.Control />
+                      <Form.Control name="city"/>
                     </Form.Group>
                     <Form.Group as={Col} controlId="formGridState">
                       <Form.Label>State</Form.Label>
-                      <Form.Control as="select">
+                      <Form.Control name="state" as="select">
                         <option>Choose...</option>
-                        <option>...</option>
+                        {
+                          this.states.map(state => (
+                            <option>{state}</option>
+                          ))
+                        }
                       </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col} controlId="formGridZip">
                       <Form.Label>Zip</Form.Label>
-                      <Form.Control />
+                      <Form.Control name="zip"/>
                     </Form.Group>
                   </Form.Row>
 
                   <Form.Row>
                     <Form.Group as={Col} controlId="exampleForm.ControlTextarea1">
                       <Form.Label>Bio</Form.Label>
-                      <Form.Control as="textarea" rows="3" />
+                      <Form.Control name="description" as="textarea" rows="3" value={this.state.description} onChange={this.handleChange}/>
                     </Form.Group>
                   </Form.Row>
 
                   <Form.Row>
                     <Form.Group as={Col}>
                       <Form.Label>Organization</Form.Label>
-                      <Form.Control type="text" />
+                      <Form.Control name="organization" type="text" value={this.state.organization} onChange={this.handleChange}/>
                     </Form.Group>
                     <Form.Group as={Col}>
                       <Form.Label>Profession</Form.Label>
-                      <Form.Control type="text" />
+                      <Form.Control name="profession" type="text" value={this.state.profession} onChange={this.handleChange}/>
                     </Form.Group>
                   </Form.Row>
                   <Form.Row>
                     <Form.Group as={Col}>
                       <Form.Label>Resume</Form.Label>
-                      <Form.Control type="file" />
+                      <Form.Control name="resume" type="file" />
                     </Form.Group>
                   </Form.Row>
                   <Form.Row>
@@ -108,7 +203,7 @@ class EditAccount extends React.Component {
                           </Card.Header>
                           <Accordion.Collapse eventKey="0">
                             <Card.Body>
-                              <Form.Control type="file" />
+                              <Form.Control name="image" type="file" />
                             </Card.Body>
                           </Accordion.Collapse>
                         </Card>
@@ -121,24 +216,29 @@ class EditAccount extends React.Component {
                           <Accordion.Collapse eventKey="1">
                             <Card.Body>
                               <Form.Label>Link to Image</Form.Label>
-                              <Form.Control type="text" />
+                              <Form.Control name="imageUrl" type="text" value={this.state.imageUrl} onChange={this.handleChange}/>
                             </Card.Body>
                           </Accordion.Collapse>
                         </Card>
                       </Accordion>
                     </Form.Group>
                   </Form.Row>
+                  {this.state.profileErrors.map(errorCode => (
+                    <Alert variant={"warning"}>
+                      {this.errors[errorCode]}
+                    </Alert>
+                  ))}
                   <Button variant="primary" type="submit" className="edit-form-btn">
                     Submit
                   </Button>
                 </Form>
               </Tab>
               <Tab eventKey="contact" title="Contact" className="profileTab">
-                <Form>
+                <Form onSubmit={this.contactSubmit.bind(this)}>
                   <Form.Row>
                     <Form.Group as={Col} controlId="formGroupEmail">
                       <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" />
+                      <Form.Control name="email" type="email" value={this.state.email} name="email" onChange={this.handleChange} />
                       <Form.Text className="text-muted" style={{marginLeft: '3px'}}>
                         We'll never share your email with anyone.
                       </Form.Text>
@@ -148,28 +248,33 @@ class EditAccount extends React.Component {
                   <Form.Row>
                     <Form.Group as={Col}>
                       <Form.Label>Phone</Form.Label>
-                      <Form.Control type="tel" />
+                      <Form.Control name="phone" type="tel" />
                     </Form.Group>
                   </Form.Row>
 
                   <Form.Row>
                     <Form.Group as={Col} controlId="a">
                       <Form.Label>LinkedIn</Form.Label>
-                      <Form.Control type="tel" />
+                      <Form.Control name="linkedin" type="text" />
                     </Form.Group>
                   </Form.Row>
 
+                  {this.state.contactErrors.map(errorCode => (
+                    <Alert variant={"warning"}>
+                      {this.errors[errorCode]}
+                    </Alert>
+                  ))}
                   <Button variant="primary" type="submit" className="edit-form-btn">
                     Submit
                   </Button>
                 </Form>
               </Tab>
               <Tab eventKey="security" title="Security" className="profileTab">
-                <Form>
+                <Form onSubmit={this.passwordSubmit.bind(this)}>
                   <Form.Row>
                     <Form.Group as={Col} controlId="formBasicPassword">
                       <Form.Label>Current Password</Form.Label>
-                      <Form.Control className="form-element" name="password" type="password"/>
+                      <Form.Control className="form-element" name="passwordold" type="password"/>
                     </Form.Group>
                   </Form.Row>
                   <Form.Row>
@@ -182,6 +287,11 @@ class EditAccount extends React.Component {
                       <Form.Control className="form-element" name="confirm" type="password"/>
                     </Form.Group>
                   </Form.Row>
+                  {this.state.passwordErrors.map(errorCode => (
+                    <Alert variant={"warning"}>
+                      {this.errors[errorCode]}
+                    </Alert>
+                  ))}
                   <Button variant="primary" type="submit" className="edit-form-btn">
                     Submit
                   </Button>
